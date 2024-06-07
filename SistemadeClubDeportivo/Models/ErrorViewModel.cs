ef0ea943c42620d20f84dc5e3f13c90b144eca09
JhamirLucas <Lucasma231@gmail.com>
2024-06-07 14:38:02 -0400

    Agregar archivos de proyecto.

---
+++ SistemadeClubDeportivo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SistemadeClubDeportivo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
